aef935d16b2675d528e01c85fd1d7a618db05070
bemrekalkan <bemre.kalkan@gmail.com>
2022-07-13 22:24:16 +0300

    navbar

--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -1,7 +1,22 @@
 import NavbarStyle from "./Navbar.style";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav>
+      <div>
+        <i>{"<Clarusway/>"}</i>
+        <span>Recipe</span>
+      </div>
+
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/register">Register</Link>
+        <Link to="/logout">Logout</Link>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
