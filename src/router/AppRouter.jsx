56223c33677c91a3b63e1d6cf3e084033f19844b
bemrekalkan <bemre.kalkan@gmail.com>
2022-07-13 20:53:49 +0300

    AppRouter

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,12 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { GlobalStyles } from "../comonents/globalStyles/Global.styles";
 import Navbar from "../comonents/nav/Navbar";
+import Home from "../pages/home/Home";
+import Login from "../pages/login/Login";
+import About from "../pages/about/About";
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
+      <GlobalStyles />
       <Navbar />
       <Routes>
-        <Route />
+        <Route path="/" element={<Home />} />
+        <Route path="login" element={<Login />} />
+        <Route path="about" element={<About />} />
       </Routes>
     </BrowserRouter>
   );
