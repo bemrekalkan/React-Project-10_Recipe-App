ebe91ebe6e121da9c6272b1d2ff4f9e478ae0ec0
bemrekalkan <bemre.kalkan@gmail.com>
2022-07-13 22:03:17 +0300

    login

--- src/components/globalStyles/Flex.jsx
+++ src/components/globalStyles/Flex.jsx
@@ -1,11 +1,10 @@
-// escd 👇
 import styled from "styled-components";
 
 const Flex = styled.div`
   display: flex;
-  justify-content: ${(justify) => (justify ? justify : "center")};
-  align-items: ${(align) => (align ? align : "center")};
-  flex-wrap: ${(wrap) => wrap && wrap};
+  justify-content: ${({ justify }) => (justify ? justify : "center")};
+  align-items: ${({ align }) => (align ? align : "center")};
+  flex-wrap: ${({ wrap }) => wrap && wrap};
 `;
 
 export default Flex;
