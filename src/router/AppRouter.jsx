af1fdb6aee44870a428366ccb0f7039014ef4a6e
bemrekalkan <bemre.kalkan@gmail.com>
2022-07-13 20:49:47 +0300

    globalstyle

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,7 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "../comonents/nav/Navbar";
 
 const AppRouter = () => {
-  return <div>AppRouter</div>;
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default AppRouter;
