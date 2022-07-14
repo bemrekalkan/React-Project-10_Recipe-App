146bc0d1c9369014272288a2c712a0e239eac36c
bemrekalkan <bemre.kalkan@gmail.com>
2022-07-14 21:19:50 +0300

    home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,31 +2,31 @@ import HomeStyle from "./Home.style";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import Header from "../../components/header/Header";
-
+import { Card } from "../../components/cards/Cards.style";
+import Cards from "../../components/cards/Cards";
 const Home = () => {
   const mealType = ["Breakfast", "Lunch", "Dinner", "Snack", "Teatime"];
   const [query, setQuery] = useState("egg");
   const [selectedMeal, setSelectedMeal] = useState(mealType[0]);
   const [recipes, setRecipes] = useState("");
   const APP_ID = `ebe4dc0a`;
-  const APP_KEY = `bd2b4d21cff9784aa4ababb3fd7bf856`;
-  const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=$
-  {APP_KEY}&mealType=${selectedMeal}`;
+  const APP_KEY = `fdefa17787eb472fab4d630daa04fbef`;
+  const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&mealType=${selectedMeal}`;
 
   const getData = async () => {
-    try {
-      //! {data} => Destructring
-      const { data } = await axios.get(url);
-      setRecipes(data.hits);
-    } catch (error) {
-      console.log(error);
+    if (query) {
+      try {
+        const { data } = await axios.get(url);
+        setRecipes(data.hits);
+      } catch (error) {
+        console.log(error);
+      }
+    } else {
+      alert("Please enter your meal");
     }
   };
 
-  useEffect(() => {
-    getData();
-  }, []);
-
+  // console.log(recipes);
   return (
     <div>
       <Header
@@ -35,6 +35,7 @@ const Home = () => {
         mealType={mealType}
         getData={getData}
       />
+      <Cards recipes={recipes} />
     </div>
   );
 };
