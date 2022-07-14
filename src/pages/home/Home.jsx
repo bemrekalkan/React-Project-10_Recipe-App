110d2358237b47322cc73d70f076b57fd5373da7
bemrekalkan <bemre.kalkan@gmail.com>
2022-07-14 20:31:27 +0300

    home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,16 +1,24 @@
 import HomeStyle from "./Home.style";
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const Home = () => {
+  const [query, setQuery] = useState("egg");
+  const [selectedMeal, setSelectedMeal] = useState("breakfast");
+  const [recipes, setRecipes] = useState("");
   const APP_ID = `ebe4dc0a`;
   const APP_KEY = `bd2b4d21cff9784aa4ababb3fd7bf856`;
   const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=$
   {APP_KEY}&mealType=${selectedMeal}`;
 
   const getData = async () => {
-    //! {data} => Destructring
-    const { data } = await axios.get(url);
+    try {
+      //! {data} => Destructring
+      const { data } = await axios.get(url);
+      setRecipes(data.hits);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   useEffect(() => {
