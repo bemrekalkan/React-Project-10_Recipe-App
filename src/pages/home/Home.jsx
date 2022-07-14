8d5f20f1f1346149c5acdd1ffae93a2178463827
bemrekalkan <bemre.kalkan@gmail.com>
2022-07-14 20:24:08 +0300

    home.jsx axios

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,23 @@
 import HomeStyle from "./Home.style";
+import axios from "axios";
+import { useEffect } from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  const APP_ID = `ebe4dc0a`;
+  const APP_KEY = `bd2b4d21cff9784aa4ababb3fd7bf856`;
+  const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=$
+  {APP_KEY}&mealType=${selectedMeal}`;
+
+  const getData = async () => {
+    //! {data} => Destructring
+    const { data } = await axios.get(url);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return <div></div>;
 };
 
 export default Home;
