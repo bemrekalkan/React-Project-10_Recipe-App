d65db66043f6bc5a11d289ee1998eb14d2c9b0ce
bemrekalkan <bemre.kalkan@gmail.com>
2022-07-14 22:07:36 +0300

    assets updated

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -1,7 +1,23 @@
-import CardsStyle from "./Cards.style";
+import { Card, Header, MainContainer, Image, Button } from "./Cards.style";
+import defaultImage from "../../assets/default-image.jpg";
+import { useNavigate } from "react-router";
 
-const Cards = () => {
-  return <div>Cards</div>;
+const Cards = ({ recipes }) => {
+  return (
+    <MainContainer wrap="wrap">
+      {recipes.map(({ recipe }, index) => (
+        <Card key={index}>
+          <Header>{recipe.label}</Header>
+          <Image src={recipe.image || defaultImage} />
+          <Button
+            onClick={(() => navigate("detail", { state: recipe }), replace)}
+          >
+            View More
+          </Button>
+        </Card>
+      ))}
+    </MainContainer>
+  );
 };
 
 export default Cards;
